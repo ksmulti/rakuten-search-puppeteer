83e5adb236096760068ac100d0489cd54d36c356
ksmulti <ksmulti@gmail.com>
2022-03-18 22:53:42 +0900

    Add RakutenSearch class

--- example.js
+++ example.js
@@ -1,5 +1,26 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
+const fs = require('fs');
+
+class RakutenSearch {
+    constructor(page) {
+        this.page = page;
+        const settingsString = fs.readFileSync('settings.json');
+        this.settings = JSON.parse(settingsString);
+    }
+
+    async loginPage() {
+        await this.page.goto('https://grp03.id.rakuten.co.jp/rms/nid/login?service_id=r12&return_url=login?tool_id=1&tp=&id=');
+        await this.page.waitForSelector('#loginInner_u');
+    }
+
+    async login() {
+        await this.page.type('#loginInner_u', this.settings.account, { delay: 20 });
+        await this.page.type('#loginInner_p', this.settings.password, { delay: 20 });
+        await this.page.click('input[name="submit"]');
+        await this.page.waitForSelector('#search-input');
+    }
+}
 
 (async () => {
     const args = puppeteer.defaultArgs().filter((arg) => String(arg).toLowerCase() !== '--disable-extensions');
@@ -14,11 +35,9 @@ const path = require('path');
     });
 
     const page = await browser.newPage();
-    await page.goto('https://grp03.id.rakuten.co.jp/rms/nid/login?service_id=r12&return_url=login?tool_id=1&tp=&id=');
-    await page.waitForSelector('#loginInner_u');
-
-    await page.type('#loginInner_u', 'test comment', { delay: 20 });
-    await page.type('#loginInner_p', 'aaa', { delay: 20 });
+    const rakutenSearch = new RakutenSearch(page);
+    await rakutenSearch.loginPage();
+    await rakutenSearch.login();
 
     await page.screenshot({ path: './screenshot/example.png' });
 
