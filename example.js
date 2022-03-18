453b07d257feaec233c7c5af0c873c10c77e2a9b
ksmulti <ksmulti@gmail.com>
2022-03-18 22:13:39 +0900

    add eslint

--- example.js
+++ example.js
@@ -1,22 +1,25 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 (async () => {
-    // const browser = await puppeteer.launch();
-    const args = puppeteer.defaultArgs().filter(arg => String(arg).toLowerCase() !== '--disable-extensions');
-    const pathToExtension = require('path').join(__dirname, 'rakuten_extension.crx');
-    console.log(pathToExtension);
+    const args = puppeteer.defaultArgs().filter((arg) => String(arg).toLowerCase() !== '--disable-extensions');
+    const pathToExtension = path.join(__dirname, 'rakuten_extension.crx');
     const browser = await puppeteer.launch({
         headless: false,
         ignoreDefaultArgs: true,
         args: args.concat([
             '--remote-debugging-port=9223',
-            `--load-extension=${pathToExtension}`
-        ])
+            `--load-extension=${pathToExtension}`,
+        ]),
     });
 
     const page = await browser.newPage();
     await page.goto('https://grp03.id.rakuten.co.jp/rms/nid/login?service_id=r12&return_url=login?tool_id=1&tp=&id=');
     await page.waitForSelector('#loginInner_u');
+
+    await page.type('#loginInner_u', 'test comment', { delay: 20 });
+    await page.type('#loginInner_p', 'aaa', { delay: 20 });
+
     await page.screenshot({ path: './screenshot/example.png' });
 
     await browser.close();
